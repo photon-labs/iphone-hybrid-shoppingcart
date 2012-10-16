b56d4782747cda12b37d61280e5f2e24029c7ee3
rojaramani-s <rojaramani.singani@photoninfotech.net>
2012-10-16 18:15:27 +0530

    Added new unit test case for iPhoneHybrid Eshop project

---
+++ source/AllTest/AllTest.h
@@ -0,0 +1,17 @@
+//
+//  AllTest.h
+//  AllTest
+//
+//  Created by Rojaramani on 16/10/12.
+//
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+#import <UIKit/UIKit.h>
+
+@interface AllTest : SenTestCase
+{
+   
+}
+
+@end
---
+++ source/AllTest/AllTest.m
@@ -0,0 +1,31 @@
+//
+//  AllTest.m
+//  AllTest
+//
+//  Created by Rojaramani on 16/10/12.
+//
+//
+
+#import "AllTest.h"
+
+@implementation AllTest
+
+- (void)setUp
+{
+    [super setUp];
+    
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in AllTest");
+}
+
+@end
