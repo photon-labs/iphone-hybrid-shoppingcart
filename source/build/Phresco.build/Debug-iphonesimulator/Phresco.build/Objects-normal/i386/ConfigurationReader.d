f42b7c0013d7d420d590ca1532fe5148b20feb62
cshiva <shivakumar.chandramouli@photoninfotech.net>
2013-04-15 17:23:32 +0530

    removing build dir

--- source/build/Phresco.build/Debug-iphonesimulator/Phresco.build/Objects-normal/i386/ConfigurationReader.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/shivakumar_ch/Documents/Projects/Eshop-hybrid-bug-fix/branch2.3-test/iphone-hybrid-shoppingcart/source/Thirdparty/ConfigurationReader/ConfigurationReader.m \
-  /Users/shivakumar_ch/Documents/Projects/Eshop-hybrid-bug-fix/branch2.3-test/iphone-hybrid-shoppingcart/source/Thirdparty/ConfigurationReader/ConfigurationReader.h
--- source/build/Phresco.build/Debug-iphonesimulator/Phresco.build/Objects-normal/i386/main.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/shivakumar_ch/Documents/Projects/Eshop-hybrid-bug-fix/branch2.3-test/iphone-hybrid-shoppingcart/source/Phresco/main.m
