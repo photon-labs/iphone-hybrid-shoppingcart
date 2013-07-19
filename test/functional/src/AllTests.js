09d0b570dfd02bea45781038f3dbbffee80fb674
uma_j <uma.jayanthi@photoninfotech.net>
2013-07-19 19:39:00 +0530

    funcitonal test changes

--- test/functional/src/AllTests.js
+++ test/functional/src/AllTests.js
@@ -1,6 +1,5 @@
 #import "main/com/photon/phresco/util/MainActivity.js"
 #import "main/com/photon/phresco/util/UIElements.js"
-#import "test/com/photon/phresco/testcase/MovieAndMusic_suite.js"
 #import "test/com/photon/phresco/testcase/Television_suite.js"
 #import "test/com/photon/phresco/testcase/Computer_suite.js"
 #import "test/com/photon/phresco/testcase/MobilePhone_suite.js"
